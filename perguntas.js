6ad26f5be112dba04e0e665d387e4e8d4243f45a
texasutilizador <00001114666531sp@al.educacao.sp.gov.br>
2024-11-29 07:18:22 -0300

    aaaaaaaaaaaaaaaaaaaa111111111111

---
+++ perguntas.js
@@ -0,0 +1,58 @@
+criaCartao(
+    '😨',
+    'quem está em paris?',
+    '😨😨😨'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'criptozoologia',
+    'onde fica o monstro do lago ness?',
+    'no lago ness'
+)
+
+criaCartao(
+    'criptologia',
+    'AAA AAB AAC AAD...',
+    'AAE AAF AAG...'
+)
+criaCartao(
+    'ingles',
+    'Como se fala cabana Inglês?',
+    'cabin'
+)
+criaCartao(
+    'filosofia',
+    'a ética é...',
+    'o conceito de humanidade'
+)
+criaCartao(
+    'geral',
+    '1908',
+    'é o ano onde a máquina de café foi criada'
+)
+criaCartao(
+    'temporal',
+    'que dia é hoje?',
+    'no futuro de após 28.11.24 '
+)
+criaCartao(
+    'geral',
+    'Qual é a palavra que caracteriza uma comida doce e salgada?',
+    'agridoce'
+)
+criaCartao(
+    'matematica',
+    'quanto é 12x12?',
+    '144'
+)
+criaCartao(
+    'ingles',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI; pronuncia: RAI'
+)
